bc0cc60a5115a0f371edeeee448a49d63b23f9f6
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2020-08-09 00:56:12 +0500

    Added message variable for displaying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 function App() {
+  const [message, setMessage] = useState('Loading...');
+  useEffect(() => {
+    async function loadPolka() {
+      const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+      const api = await ApiPromise.create({ provider: wsProvider });
+      setMessage(api.genesisHash.toHex());
+    }
+    loadPolka();
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{message}</p>
       </header>
     </div>
   );
